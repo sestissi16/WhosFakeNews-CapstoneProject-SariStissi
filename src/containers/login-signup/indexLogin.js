56ad63516d136bc3c4d0886ef844c40526b83a41
Sari Stissi <sestissi16@earlham.edu>
2020-04-01 03:45:17 -0500

    added orange decorative sidebars to the login-signup page

--- src/containers/login-signup/indexLogin.js
+++ src/containers/login-signup/indexLogin.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import './indexLogin.css'
 
 const LoginSignUp = () => (
-  <div>
-    <h1>Login or Sign Up</h1>
-    <p>This is where you can login to save the results of the fake news testing.</p>
-  </div>
+    <div id="loginPageContainer">
+        <div id="loginLeftSideBar"></div>
+        <div id="loginContent">
+            <div id="loginHeader">
+                <h1>Login or Sign Up</h1>
+            </div>
+            <p>This is where you can login to save the results of the fake news testing.</p>
+        </div>
+        <div id="loginRightSideBar"></div>
+    </div>
 )
 
 export default LoginSignUp
