b98fc850f62b5505175b2aeb9672bd23d3d9d03b
Sari Stissi <sestissi16@earlham.edu>
2020-04-01 03:39:38 -0500

    added green decorative sidebars to extension page and changed the sidebar flex grows to widths in all of them

--- src/containers/fakeNewsDetectionOptions/indexExtension.js
+++ src/containers/fakeNewsDetectionOptions/indexExtension.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import './indexExtension.css'
 
 const Extension = () => (
-  <div>
-    <h1>Extension Page</h1>
-    <p>
-        This is where you find out what the extension does, see more detailed information from the extension, 
-        and find instructions on how to install it.
-    </p>
-  </div>
+    <div id="extensionPageContainer">
+        <div id="extensionLeftSideBar"></div>
+        <div id="extensionContent">
+            <div id="extensionHeader">
+                <h1>Extension Page</h1>
+            </div>
+            <p>
+                This is where you find out what the extension does, 
+                see more detailed information from the extension, 
+                and find instructions on how to install it.
+            </p>
+        </div>
+        <div id="extensionRightSideBar"></div>
+    </div>
 )
 
 export default Extension
