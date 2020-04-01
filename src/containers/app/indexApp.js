93d519bf177d713dd806ea0caa77a33d85f8b9da
Sari Stissi <sestissi16@earlham.edu>
2020-03-31 22:18:50 -0500

    fixed react router issues with Router container tag and importing BrowserRouter from react-router-dom

--- src/containers/app/indexApp.js
+++ src/containers/app/indexApp.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, BrowserRouter as Router } from 'react-router-dom'
 import {Navbar, Nav, Form, Button, FormControl} from 'react-bootstrap'
 import Home from '../home'
 import About from '../about'
@@ -9,22 +9,23 @@ const App = () => (
   <div id="containerApp">
     <Navbar bg="dark" variant="dark">
     <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="mr-auto">
+    <Nav variatn="pills" className="mr-auto">
       <Nav.Link href="/">Home</Nav.Link>
       <Nav.Link href="/features">Features</Nav.Link>
-      <Nav.Link href="/pricing">Pricing</Nav.Link>
+      <Nav.Link href="/about">About</Nav.Link>
     </Nav>
     <Form inline>
       <FormControl type="text" placeholder="Search" className="mr-sm-2" />
       <Button variant="outline-info">Search</Button>
     </Form>
     </Navbar>
-
+    <Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+    </Router>
 
     <main>
       
-      {/* <Route exact path="/" component={Home} /> */}
-      {/* <Route exact path="/about-us" component={About} /> */}
     </main>
   </div>
 )
