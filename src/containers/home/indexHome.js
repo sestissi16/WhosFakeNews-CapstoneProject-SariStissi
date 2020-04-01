826b5aea19a3daf0ecc47b922fa69ee6809e2ce0
Sari Stissi <sestissi16@earlham.edu>
2020-04-01 16:04:47 -0500

    moved the sidebars down below the banner header

--- src/containers/home/indexHome.js
+++ src/containers/home/indexHome.js
@@ -4,17 +4,19 @@ import './indexHome.css'
 
 const Home = props => (
   <div id="homePageContainer">
-    <div id="homeLeftSideBar"></div>
     <div id="homeContent">
       <div id="homeHeader">
         <h6 id="homeHeaderMessage"><strong><em>Start uncovering fake news now by creating a FREE account</em></strong></h6>
         <Button href="/login" variant="danger" id="homeHeaderSignUp">Sign Up</Button>
       </div>
       <div id="homeBody">
-        <p>Here's more information about this website and what you can do with it.</p>
+        <div id="homeLeftSideBar"></div>
+        <div id="homeBodyContent">
+          <p>Here's more information about this website and what you can do with it.</p>
+        </div>
+        <div id="homeRightSideBar"></div>
       </div>
     </div>
-    <div id="homeRightSideBar"></div>
   </div>
 )
 
