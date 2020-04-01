30bfffd684c58c48f69d6990893a8eb844e62b2f
Sari Stissi <sestissi16@earlham.edu>
2020-04-01 02:43:04 -0500

    added the same decorative sidebars to upload page but in yellow and tweaked the responsiveness in both the home and upload pages

--- src/containers/fakeNewsDetectionOptions/indexUpload.js
+++ src/containers/fakeNewsDetectionOptions/indexUpload.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import './indexUpload.css'
 
 const Upload = () => (
-  <div>
-    <h1>Upload Page</h1>
-    <p>This is where you can upload a piece of media and have it checked.</p>
+  <div id="uploadPageContainer">
+    <div id="uploadLeftSideBar"></div>
+    <div id="uploadContent">
+      <div id="uploadHeader">
+        <h1>Upload Page</h1>
+      </div>
+      <p>This is where you can upload a piece of media and have it checked.</p>
+    </div>
+    <div id="uploadRightSideBar"></div>
   </div>
 )
 
