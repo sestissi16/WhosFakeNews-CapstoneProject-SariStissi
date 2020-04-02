4c84807dadf07c57eae262e9c12ad067b2631ce2
Sari Stissi <sestissi16@earlham.edu>
2020-04-01 19:52:34 -0500

    fixed the bad height and image size issues with the homepage

--- src/containers/home/indexHome.js
+++ src/containers/home/indexHome.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button } from 'react-bootstrap'
-import FakeNewsImage from '../../Media/CS488-freeFakeNewsImage.jpg'
+import Image from 'react-bootstrap/Image'
+import FakeNewsImage from '../../Media/CS488-smallFreeFakeNewsImage.jpg'
 import './indexHome.css'
 
 const Home = props => (
@@ -15,7 +16,7 @@ const Home = props => (
         <div id="homeBodyContent">
           <div id="homeBodyTop">
             <div id="homeBodyTopImage">
-              <img id="homeFakeNewsImage" src={FakeNewsImage} alt="a desk with a computer where the words fake news displayed" />
+              <img id="homeFakeNewsImage" src={FakeNewsImage} alt="a desk with a computer where the words fake news displayed" fluid/>
               <p id="homeBodyTopCredit">Credit: Image by 
                 <a href="https://pixabay.com/users/pixel2013-2364555/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1909821">
                   S. Hermann &amp; F. Richter
@@ -25,7 +26,13 @@ const Home = props => (
                 </a>
               </p>
             </div>
-            <p id="homeBodyTopTitle"><strong>Find out whether something is Fake News with ease</strong></p>
+            <div id="homeBodyTopInfo">
+              <p id="homeBodyTopTitle"><strong>Find out whether something is Fake News with ease</strong></p>
+              <p id="homeBodyTopSub">
+                Welcome to the one-stop shop of fake news detection. 
+                Our detection is accurate as well as easy to use with three different ways to get results.
+              </p>
+            </div>
           </div>
         </div>
         <div id="homeRightSideBar"></div>
