751aa82365a91369f8dca49674c0b57bfa65f77e
Sari Stissi <sestissi16@earlham.edu>
2020-04-01 21:10:40 -0500

    added a form to the login section

--- src/containers/login-signup/indexLogin.js
+++ src/containers/login-signup/indexLogin.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Form, InputGroup } from 'react-bootstrap'
 import './indexLogin.css'
 
 const LoginSignUp = () => (
@@ -10,7 +11,30 @@ const LoginSignUp = () => (
             </div>
             <div id="loginBody">
                 <div id="loginSection">
-                    <p>Placeholder text for login section</p>
+                    <Form>
+                        <Form.Group controlId="formGroupEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                        </Form.Group>
+                        <Form.Group controlId="groupCustomUsername">
+                            <Form.Label>Username</Form.Label>
+                            <InputGroup>
+                                <InputGroup.Prepend>
+                                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <Form.Control
+                                type="text"
+                                placeholder="Username"
+                                aria-describedby="inputGroupPrepend"
+                                required
+                                />
+                            </InputGroup>
+                        </Form.Group>
+                        <Form.Group controlId="formGroupPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                    </Form>
                 </div>
                 <div id="signUpSection">
                     <p>Placeholder text for the sign up section</p>
