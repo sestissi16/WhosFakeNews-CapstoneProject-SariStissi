77695ea352d9496dce951ca4f1659a54a4c4c035
Sari Stissi <sestissi16@earlham.edu>
2020-04-16 23:46:11 -0500

    changed upload page to match the others

--- src/containers/fakeNewsDetectionOptions/indexUpload.js
+++ src/containers/fakeNewsDetectionOptions/indexUpload.js
@@ -3,16 +3,18 @@ import './indexUpload.css'
 
 const Upload = () => (
   <div id="uploadPageContainer">
-    <div id="uploadLeftSideBar"></div>
     <div id="uploadContent">
-      <div id="uploadHeader">
-        <h1>Upload Page</h1>
-      </div>
-      <div id="uploadBody">
-        <p>This is where you can upload a piece of media and have it checked.</p>
-      </div>
+        <div id="uploadHeader">
+            <h6 id="uploadHeaderMessage"><strong><em>Upload Page</em></strong></h6>
+        </div>
+        <div id="uploadBody">
+            <div id="uploadLeftSideBar"></div>
+            <div id="uploadBodyContent">
+                <p>This is where you can upload a piece of media and have it checked.</p>
+            </div>
+            <div id="uploadRightSideBar"></div>
+        </div>
     </div>
-    <div id="uploadRightSideBar"></div>
   </div>
 )
 
