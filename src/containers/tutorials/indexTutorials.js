834a372b960282a19758f198339e7cf2be36325c
Sari Stissi <sestissi16@earlham.edu>
2020-04-16 22:18:24 -0500

    added the sidebars and header to tutorial page

--- src/containers/tutorials/indexTutorials.js
+++ src/containers/tutorials/indexTutorials.js
@@ -4,8 +4,18 @@ import './indexTutorials.css'
 
 const Tutorials = () => (
     <div id="tutorialsPageContainer">
-        <h1>Tutorials Page</h1>
-
+        <div id="tutorialsContent">
+            <div id="tutorialsHeader">
+                <h6 id="tutorialsHeaderMessage"><strong><em>Tutorials Page</em></strong></h6>
+            </div>
+            <div id="tutorialsBody">
+                <div id="tutorialsLeftSideBar"></div>
+                <div id="tutorialsBodyContent">
+                    <p>Content for tutorials</p>
+                </div>
+                <div id="tutorialsRightSideBar"></div>
+            </div>
+        </div>
     </div>
 )
 
