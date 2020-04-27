fff7562797e42642fabad5927553c9747d39a974
Sari Stissi <sestissi16@earlham.edu>
2020-04-26 23:45:36 -0500

    used fit-content property and aligned getStarted div to flex-start to fix sizing issue

--- src/containers/home/indexHome.js
+++ src/containers/home/indexHome.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button } from 'react-bootstrap'
+import { Divider } from 'semantic-ui-react'
 import Image from 'react-bootstrap/Image'
 import HomepageImage from '../../Media/CS488-MicrophoneCartoonOfNews-Cropped.png'
 // import FakeNewsImage from '../../Media/CS488-smallFreeFakeNewsImage.jpg'
