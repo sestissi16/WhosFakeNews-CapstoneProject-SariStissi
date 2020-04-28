43bdc643e87c94dcbe88bd3fe430aba1e2ca8b6d
Sari Stissi <sestissi16@earlham.edu>
2020-04-28 18:09:42 -0500

    added newsAlert info, styling change & fix grammar

--- src/containers/options/indexOptions.js
+++ src/containers/options/indexOptions.js
@@ -18,14 +18,21 @@ const Options = () => (
                         <div id="optionsBodyDescriptionsInfo">
                             <div id="optionsBodyDescriptInfoUploads">
                                 <h5>Uploads</h5>
-                                <p>Where you can upload files or paste URL links and we'll check them for fake news</p>
+                                <p>Where you can upload files or paste URL links, and we'll check them for fake news</p>
                                 <Button href="/Options/Uploads" variant="light" id="OptionsUploadButton">Go to Uploads</Button>
                             </div>
                             <div id="optionsBodyDescriptInfoAlerts">
-                                <p>Info about News Alerts</p>
+                                <h5>News Alerts</h5>
+                                <p>Where you can add keywords and topics you want us to keep an eye out for in the news. 
+                                    If we find something, we'll check it for you and notify you of the results.</p>
+                                <Button href="/Options/NewsAlerts" variant="light" id="OptionsAlertsButton">Go to News Alerts</Button>
                             </div>
                             <div id="optionsBodyDescriptInfoExtension">
-                                <p>Info about the Extension</p>
+                                <h5>Web Browser Extension</h5>
+                                <p>First, download a browser extension. 
+                                    The extension will check if the tab has any info to verify. 
+                                    It will show you the results and you don't have to press a thing!</p>
+                                <Button href="/Options/Extension" variant="light" id="OptionsExtensionButton">Go to Extension</Button>
                             </div>
                         </div>
                         <div id="optionsBodyDescriptionsNotice">
