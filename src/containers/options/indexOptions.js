22cd52b89666ac68c587e2fb1236ac03a15f49d8
Sari Stissi <sestissi16@earlham.edu>
2020-04-28 17:07:36 -0500

    added div containers to options main page with headers

--- src/containers/options/indexOptions.js
+++ src/containers/options/indexOptions.js
@@ -6,11 +6,29 @@ const Options = () => (
     <div id="optionsPageContainer">
         <div id="optionsContent">
             <div id="optionsHeader">
-                <h6 id="optionsHeaderMessage"><strong><em>Options Page</em></strong></h6>
+                <h3 id="optionsHeaderMessage"><strong><em>Here are the different options for finding fake news</em></strong></h3>
             </div>
             <div id="optionsBody">
                 <div id="optionsBodyContent">
-                    <p>Content for fake news options landing page</p>
+                    <div id="optionsBodyDescriptions">
+                        <div id="optionsBodyDescriptionsTitle">
+                            <h1>We explain each of the ways we help you find fake news below:</h1>
+                        </div>
+                        <div id="optionsBodyDescriptionsInfo">
+                            <div id="optionsBodyDescriptInfoUploads">
+                                <p>Info about uploads</p>
+                            </div>
+                            <div id="optionsBodyDescriptInfoAlerts">
+                                <p>Info about News Alerts</p>
+                            </div>
+                            <div id="optionsBodyDescriptInfoExtension">
+                                <p>Info about the Extension</p>
+                            </div>
+                        </div>
+                        <div id="optionsBodyDescriptionsNotice">
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
