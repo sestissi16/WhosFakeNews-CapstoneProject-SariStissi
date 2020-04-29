ccb2788b64ab799a500f100fa406d12755925ab0
Sari Stissi <sestissi16@earlham.edu>
2020-04-29 00:23:51 -0500

    added Filepond to do the file upload part of this project for now at the very least

--- src/containers/fakeNewsDetectionOptions/indexUpload.js
+++ src/containers/fakeNewsDetectionOptions/indexUpload.js
@@ -1,18 +1,26 @@
 import React from 'react'
+import { FilePond } from 'react-filepond';
+import 'filepond/dist/filepond.min.css';
 import './indexUpload.css'
 
 const Upload = () => (
   <div id="uploadPageContainer">
     <div id="uploadContent">
         <div id="uploadHeader">
-            <h6 id="uploadHeaderMessage"><strong><em>Upload Page</em></strong></h6>
+            <h3 id="uploadHeaderMessage"><strong><em>Upload what you want to check for fake news</em></strong></h3>
         </div>
         <div id="uploadBody">
-            <div id="uploadLeftSideBar"></div>
             <div id="uploadBodyContent">
-                <p>This is where you can upload a piece of media and have it checked.</p>
+                <div id="uploadBodyInstructions">
+                  <h1 id="uploadInstructionsTitle">How and what to upload to check for fake news:</h1>
+                </div>
+                <div id="uploadBodyForm">
+                  <FilePond/>
+                </div>
+                <div id="uploadBodyResults">
+
+                </div>
             </div>
-            <div id="uploadRightSideBar"></div>
         </div>
     </div>
   </div>
