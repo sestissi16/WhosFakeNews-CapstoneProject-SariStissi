1ef9f07b3e82edf9e7019afbe540d926d2e663aa
Sari Stissi <sestissi16@earlham.edu>
2020-04-29 17:32:31 -0500

    added Form input, wrappings, and stylings

--- src/containers/fakeNewsDetectionOptions/indexUpload.js
+++ src/containers/fakeNewsDetectionOptions/indexUpload.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import { Form, InputGroup, Button, Col } from 'react-bootstrap'
+import { Divider } from 'semantic-ui-react'
 import { FilePond, registerPlugin} from 'react-filepond';
 import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
 import 'filepond/dist/filepond.min.css';
 import './indexUpload.css'
 
-// registerPlugin(FilePondPluginFileValidateType);
+registerPlugin(FilePondPluginFileValidateType);
 class Upload extends Component {
 
   render() {
@@ -24,11 +26,33 @@ class Upload extends Component {
                     </div>
                     <div id="uploadBodyForm">
                       <h3 id="uploadBodyFormLabel">Upload your news below</h3>
-                      {/* <FilePond
-                        allowFileTypeValidation={true}
-                        acceptedFileTypes={['application/*','image/*','video/*']}
-                        labelFileTypeNotAllowed={'Sorry, this file type is not accepted yet'}
-                      /> */}
+                      <Form id="uploadBodyFormWrapper">
+                        <Form.Group as={Col} md="8" controlId="urlUpload">
+                          <Form.Label id="urlUploadLabel">Upload a url</Form.Label>
+                          <InputGroup className="mb-3">
+                            <Form.Control
+                              type="text"
+                              placeholder="Paste in your url"
+                              aria-label="User upload url"
+                              // aria-describedby="userUploadUrl"
+                              defaultValue=""
+                            />
+                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                            <InputGroup.Append>
+                              <Button variant="danger">Upload</Button>
+                            </InputGroup.Append>
+                          </InputGroup>
+                        </Form.Group>
+                        <h5>Or</h5>
+                        <Form.Group as={Col} md="8" controlId="filePondUpload">
+                          <Form.Label id="fileUploadLabel">Upload a file</Form.Label>
+                          <FilePond
+                            allowFileTypeValidation={true}
+                            acceptedFileTypes={['application/* doc','image/*','video/*']}
+                            labelFileTypeNotAllowed={'Sorry, this file type is not accepted yet'}
+                          />
+                        </Form.Group>
+                      </Form>
                     </div>
                     <div id="uploadBodyResults">
 
