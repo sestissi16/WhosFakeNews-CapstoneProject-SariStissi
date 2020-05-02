e31eba2327849989383d0aa126605e992e6d8a39
Sari Stissi <sestissi16@earlham.edu>
2020-05-02 17:11:54 -0500

    added result examples and styled them

--- client/src/containers/fakeNewsDetectionOptions/indexNewsAlerts.js
+++ client/src/containers/fakeNewsDetectionOptions/indexNewsAlerts.js
@@ -217,13 +217,114 @@ const NewsAlerts = () => (
                                 </div>
                             </div>
                             <div id="newsAlertsViewResults">
-                                <p>Placeholder for where you can see all the results of fake news checks
-                                    on the news relating to the keywords<br />
-                                    It will have the title, the keyword(s)  it relates to, the date found and checked, the Results
-                                    and part of the details showing depending on the size of the website<br />
-                                    You will be able to scroll through them as there are more. It will be organized by most recent first
-                                    but I will have different filter options the search bar so people can choose.<br />
-                                </p>
+                                <div id="viewResultsDisplayCard-1" class="viewResultsDisplayCard">
+                                    <div id="displayCardTopRow-1" class="displayCardTopRow">
+                                        <div id="displayCardTitle-1" class="displayCardTitle">
+                                            <h5>Article Title 1</h5>
+                                        </div>
+                                        <div id="displayCardResult-1" class="displayCardResult">
+                                            <h5>Fake</h5>
+                                        </div>
+                                        <div id="displayCardDate-1" class="displayCardDate">
+                                            <h6>4-20-2020</h6>
+                                        </div>
+                                    </div>
+                                    <div id="displayCardBottomRow-1" class="displayCardBottomRow">
+                                        <div id="displayCardDetails-1" class="displayCardDetails">
+                                            <p>Placeholder text for details from fake news check.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div id="viewResultsDisplayCard-2" class="viewResultsDisplayCard">
+                                    <div id="displayCardTopRow-2" class="displayCardTopRow">
+                                        <div id="displayCardTitle-2" class="displayCardTitle">
+                                            <h5>Photo Title 2</h5>
+                                        </div>
+                                        <div id="displayCardResult-2" class="displayCardResult">
+                                            <h5>Real</h5>
+                                        </div>
+                                        <div id="displayCardDate-2" class="displayCardDate">
+                                            <h6>4-15-2020</h6>
+                                        </div>
+                                    </div>
+                                    <div id="displayCardBottomRow-2" class="displayCardBottomRow">
+                                        <div id="displayCardDetails-2" class="displayCardDetails">
+                                            <p>Placeholder text for details from fake news check.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div id="viewResultsDisplayCard-3" class="viewResultsDisplayCard">
+                                    <div id="displayCardTopRow-3" class="displayCardTopRow">
+                                        <div id="displayCardTitle-3" class="displayCardTitle">
+                                            <h5>Article Title 3</h5>
+                                        </div>
+                                        <div id="displayCardResult-3" class="displayCardResult">
+                                            <h5>Real</h5>
+                                        </div>
+                                        <div id="displayCardDate-3" class="displayCardDate">
+                                            <h6>4-14-2020</h6>
+                                        </div>
+                                    </div>
+                                    <div id="displayCardBottomRow-3" class="displayCardBottomRow">
+                                        <div id="displayCardDetails-3" class="displayCardDetails">
+                                            <p>Placeholder text for details from fake news check.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div id="viewResultsDisplayCard-4" class="viewResultsDisplayCard">
+                                    <div id="displayCardTopRow-4" class="displayCardTopRow">
+                                        <div id="displayCardTitle-4" class="displayCardTitle">
+                                            <h5>Video Title 4</h5>
+                                        </div>
+                                        <div id="displayCardResult-4" class="displayCardResult">
+                                            <h5>Fake</h5>
+                                        </div>
+                                        <div id="displayCardDate-4" class="displayCardDate">
+                                            <h6>4-10-2020</h6>
+                                        </div>
+                                    </div>
+                                    <div id="displayCardBottomRow-4" class="displayCardBottomRow">
+                                        <div id="displayCardDetails-4" class="displayCardDetails">
+                                            <p>Placeholder text for details from fake news check.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div id="viewResultsDisplayCard-5" class="viewResultsDisplayCard">
+                                    <div id="displayCardTopRow-5" class="displayCardTopRow">
+                                        <div id="displayCardTitle-5" class="displayCardTitle">
+                                            <h5>Article Title 5</h5>
+                                        </div>
+                                        <div id="displayCardResult-5" class="displayCardResult">
+                                            <h5>Fake</h5>
+                                        </div>
+                                        <div id="displayCardDate-5" class="displayCardDate">
+                                            <h6>3-31-2020</h6>
+                                        </div>
+                                    </div>
+                                    <div id="displayCardBottomRow-5" class="displayCardBottomRow">
+                                        <div id="displayCardDetails-5" class="displayCardDetails">
+                                            <p>Placeholder text for details from fake news check.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div id="viewResultsDisplayCard-6" class="viewResultsDisplayCard">
+                                    <div id="displayCardTopRow-6" class="displayCardTopRow">
+                                        <div id="displayCardTitle-6" class="displayCardTitle">
+                                            <h5>Video Title 6</h5>
+                                        </div>
+                                        <div id="displayCardResult-6" class="displayCardResult">
+                                            <h5>Real</h5>
+                                        </div>
+                                        <div id="displayCardDate-6" class="displayCardDate">
+                                            <h6>3-25-2020</h6>
+                                        </div>
+                                    </div>
+                                    <div id="displayCardBottomRow-6" class="displayCardBottomRow">
+                                        <div id="displayCardDetails-6" class="displayCardDetails">
+                                            <p>Placeholder text for details from fake news check.</p>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
