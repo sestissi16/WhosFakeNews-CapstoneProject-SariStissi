4a0401fb8cec57d1721884a6557fad1533d14bef
Sari Stissi <sestissi16@earlham.edu>
2020-05-03 20:42:13 -0500

    added title, item component, and styled faq page

--- client/src/containers/faq/indexFaq.js
+++ client/src/containers/faq/indexFaq.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Item } from 'semantic-ui-react'
 import './indexFaq.css'
 
 
@@ -10,7 +11,63 @@ const FAQ = () => (
             </div>
             <div id="faqBody">
                 <div id="faqBodyContent">
-                    <p>Content for FAQ</p>
+                    <div id="faqBodyTitle">
+                        <h1>Frequently Asked Questions (FAQ):</h1>
+                    </div>
+                    <div id="faqBodyList">
+                        <div class="faqListCard">
+                            <Item.Group divided>
+                                <Item>
+                                    <Item.Content>
+                                        <Item.Header as='a'>Question 1</Item.Header>
+                                        <Item.Meta>
+                                            <span className='faq'>Coming soon!</span>
+                                        </Item.Meta>
+                                        <Item.Description>
+                                            This is where we would have the answer to your question!
+                                        </Item.Description>
+                                    </Item.Content>
+                                </Item>
+
+                                <Item>
+                                    <Item.Content>
+                                        <Item.Header as='a'>Question 2</Item.Header>
+                                        <Item.Meta>
+                                            <span className='faq'>Coming soon!</span>
+                                        </Item.Meta>
+                                        <Item.Description>
+                                            This is where we would have the answer to your question!
+                                        </Item.Description>
+                                    </Item.Content>
+                                </Item>
+
+                                <Item>
+                                    <Item.Content>
+                                        <Item.Header as='a'>Question 3</Item.Header>
+                                        <Item.Meta>
+                                            <span className='faq'>Coming soon!</span>
+                                        </Item.Meta>
+                                        <Item.Description>
+                                            This is where we would have the answer to your question!
+                                        </Item.Description>
+                                    </Item.Content>
+                                </Item>
+
+                                <Item>
+                                    <Item.Content>
+                                        <Item.Header as='a'>Question 4</Item.Header>
+                                        <Item.Meta>
+                                            <span className='faq'>Coming soon!</span>
+                                        </Item.Meta>
+                                        <Item.Description>
+                                            This is where we would have the answer to your question!
+                                        </Item.Description>
+                                    </Item.Content>
+                                </Item>
+
+                            </Item.Group>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
