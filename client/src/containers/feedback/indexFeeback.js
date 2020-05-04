dc37bf259f2a7b7a4ab3404d80c5c4134596536f
Sari Stissi <sestissi16@earlham.edu>
2020-05-04 15:16:04 -0500

    added form from semantic-ui to feedback page

--- client/src/containers/feedback/indexFeeback.js
+++ client/src/containers/feedback/indexFeeback.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Form, Input, TextArea, Button, Select } from 'semantic-ui-react'
 import './indexFeedback.css'
 
 
@@ -10,7 +11,52 @@ const Feedback = () => (
             </div>
             <div id="feedbackBody">
                 <div id="feedbackBodyContent">
-                    <p>Content for Feedback</p>
+                    <div id="feedbackEmailForm">
+                        <Form>
+                            <Form.Group widths='equal'>
+                                <Form.Field
+                                    id='form-input-control-first-name'
+                                    control={Input}
+                                    label='First name'
+                                    placeholder='First name'
+                                />
+                                <Form.Field
+                                    id='form-input-control-last-name'
+                                    control={Input}
+                                    label='Last name'
+                                    placeholder='Last name'
+                                />
+                            </Form.Group>
+                            <Form.Field
+                                id='form-input-control-email'
+                                control={Input}
+                                label='Email'
+                                placeholder='joe@gmail.com'
+                                // error={{
+                                //     content: 'Please enter a valid email address',
+                                //     pointing: 'below',
+                                // }}
+                            />
+                            <Form.Field
+                                id='form-input-control-subject'
+                                control={Input}
+                                label='Subject Line'
+                                placeholder='Write what you wanted to contact us about'
+                            />
+                            <Form.Field
+                                id='form-textarea-control-body'
+                                control={TextArea}
+                                label='Email body'
+                                placeholder='Where you can write whatever you wanted to send us'
+                            />
+                            <Form.Field
+                                id='form-button-control-public'
+                                control={Button}
+                                content='Send'
+                                label='Send Form'
+                            />
+                        </Form>
+                    </div>
                 </div>
             </div>
         </div>
