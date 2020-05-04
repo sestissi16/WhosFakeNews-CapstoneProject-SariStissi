772222f25cc02f46dfeffb27b89a963e1bdb07fb
Sari Stissi <sestissi16@earlham.edu>
2020-05-03 20:48:00 -0500

    removed sidebars from tutorials page

--- client/src/containers/tutorials/indexTutorials.js
+++ client/src/containers/tutorials/indexTutorials.js
@@ -9,11 +9,9 @@ const Tutorials = () => (
                 <h6 id="tutorialsHeaderMessage"><strong><em>Tutorials Page</em></strong></h6>
             </div>
             <div id="tutorialsBody">
-                <div id="tutorialsLeftSideBar"></div>
                 <div id="tutorialsBodyContent">
                     <p>Content for tutorials</p>
                 </div>
-                <div id="tutorialsRightSideBar"></div>
             </div>
         </div>
     </div>
